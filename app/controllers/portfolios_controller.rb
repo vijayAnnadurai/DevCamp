bdcba71a2e197940b07fee3d41afba4cab422e6f
Vijay Shankar Annadurai <Vijay@Vijays-MacBook.local>
2021-02-21 01:14:00 -0800

    added show action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,47 @@
 class PortfoliosController < ApplicationController
-
+  before_action :set_portfolio, only: [:new, :edit, :update, :show]
   def index
    @portfolio_items = Portfolio.all
   end
+  
+  def new
+   end 	
 
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+  def show
+  end
+
+  def set_portfolio
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+  end
 
 end
